cbc685b48e30d5bd81ea9c2bfa17e15a36ce4041
Stalin32 <denerko32@yandex.ru>
2024-09-06 15:05:52 +0300

    Убрал все sudo

--- prometheus_grafana_alertmanager.sh
+++ prometheus_grafana_alertmanager.sh
@@ -1,30 +1,30 @@
 
 #!/bin/bash
-sudo apt update
+apt update
 
 apt install wget tar
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.53.2/prometheus-2.53.2.linux-amd64.tar.gz
 
-sudo mkdir /etc/prometheus /var/lib/prometheus
+mkdir /etc/prometheus /var/lib/prometheus
 
 tar -zxf prometheus-*.linux-amd64.tar.gz
 
 cd prometheus-*.linux-amd64
 
-sudo cp prometheus promtool /usr/local/bin/
+cp prometheus promtool /usr/local/bin/
 
-sudo cp -r console_libraries consoles prometheus.yml /etc/prometheus
+cp -r console_libraries consoles prometheus.yml /etc/prometheus
 
 cd .. && rm -rf prometheus-*.linux-amd64/ && rm -f prometheus-*.linux-amd64.tar.gz
 
-sudo useradd --no-create-home --shell /bin/false prometheus
+useradd --no-create-home --shell /bin/false prometheus
 
-sudo chown -R prometheus:prometheus /etc/prometheus 
-sudo chown -R prometheus:prometheus /var/lib/prometheus
+chown -R prometheus:prometheus /etc/prometheus 
+chown -R prometheus:prometheus /var/lib/prometheus
 
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
+chown prometheus:prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/promtool
 
 echo "[Unit]" >> ~/prometheus.service
 echo "Description=Prometheus Service" >> ~/prometheus.service
@@ -36,16 +36,16 @@ echo "User=prometheus" >> ~/prometheus.service
 echo "Group=prometheus" >> ~/prometheus.service
 echo "Type=simple" >> ~/prometheus.service
 echo "ExecStart=/usr/local/bin/prometheus \ " >> ~/prometheus.service
-sudo echo " --config.file /etc/prometheus/prometheus.yml \ " >> ~/prometheus.service
-sudo echo " --storage.tsdb.path /var/lib/prometheus/ \ " >> ~/prometheus.service
-sudo echo " --web.console.templates=/etc/prometheus/consoles \ " >> ~/prometheus.service
-sudo echo " --web.console.libraries=/etc/prometheus/console_libraries" >> ~/prometheus.service
-sudo echo "ExecReload=/bin/kill -HUP $MAINPID" >> ~/prometheus.service
-sudo echo "Restart=on-failure" >> ~/prometheus.service
-sudo echo "" >> ~/prometheus.service
-sudo echo "[Install]" >> ~/prometheus.service
-sudo echo "WantedBy=multi-user.target" >> ~/prometheus.service
+echo " --config.file /etc/prometheus/prometheus.yml \ " >> ~/prometheus.service
+echo " --storage.tsdb.path /var/lib/prometheus/ \ " >> ~/prometheus.service
+echo " --web.console.templates=/etc/prometheus/consoles \ " >> ~/prometheus.service
+echo " --web.console.libraries=/etc/prometheus/console_libraries" >> ~/prometheus.service
+echo "ExecReload=/bin/kill -HUP $MAINPID" >> ~/prometheus.service
+echo "Restart=on-failure" >> ~/prometheus.service
+echo "" >> ~/prometheus.service
+echo "[Install]" >> ~/prometheus.service
+echo "WantedBy=multi-user.target" >> ~/prometheus.service
 
-sudo mv ~/prometheus.service /etc/systemd/system/
-sudo systemctl enable prometheus
-sudo systemctl start prometheus
+mv ~/prometheus.service /etc/systemd/system/
+systemctl enable prometheus
+systemctl start prometheus
