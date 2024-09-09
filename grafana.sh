bfcc5b50cb92cd5433f98213ba3b82bfa1867fd6
Stalin32 <denerko32@yandex.ru>
2024-09-09 14:32:44 +0300

    удалил отдельные алертменеджер и графану

--- grafana.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt install -y adduser libfontconfig1 musl
-wget https://dl.grafana.com/oss/release/grafana_10.2.3_amd64.deb
-dpkg -i grafana_*_amd64.deb
-systemctl enable grafana-server
-systemctl start grafana-server
-rm grafana_*_amd64.deb
